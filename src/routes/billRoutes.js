7536cf50112dcd2318a0e079c6c2c54bb0c86e93
gnozzz <126407248+gnozzz@users.noreply.github.com>
2025-02-18 21:22:59 +0000

    bill name addition and get bills by search progress

--- src/routes/billRoutes.js
+++ src/routes/billRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { uploadBill, getBillById, getBillsByField, markBillAsPaid, deleteBill, updateBill, highlightBill } = require('../controllers/billController');  
+const { uploadBill, getBillById, getBillsByField, markBillAsPaid, deleteBill, updateBill, highlightBill, getBillsBySearch } = require('../controllers/billController');  
 const router = express.Router()
 
 const { authenticateToken } = require('../middlewares/authMiddleware'); // For protected routes that get information from firestore
@@ -11,5 +11,6 @@ router.patch('/:billId/paid', authenticateToken, markBillAsPaid);
 router.patch('/:billId/saved', authenticateToken, highlightBill)
 router.put('/:billId', authenticateToken, updateBill); 
 router.delete('/:billId', authenticateToken, deleteBill)
+router.get('/billName', authenticateToken, getBillsBySearch)
 
 module.exports = router; 
